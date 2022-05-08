b2fc8ebe6b665eb06dcb29b98e96e4b914112cd3
Mina Tzana <Asimina.Tzana@gr.ey.com>
2022-05-08 12:04:29 +0300

    first commit

--- Advanced_select/New Companies.sql
+++ Advanced_select/New Companies.sql
@@ -1,3 +1,5 @@
+--Writer: Mina Tzana
+
 --Problem Statement
 
 /* Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, 
